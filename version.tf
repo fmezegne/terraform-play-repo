0f391cfe0de57d844a7d1427268df70c15ed5607
fmezegne <85536642+fmezegne@users.noreply.github.com>
2021-10-28 21:06:56 -0400

    Delete version.tf

--- version.tf
+++
@@ -1,9 +0,0 @@
-##### terraform block
-
-terraform {
-  backend "s3" {
-    bucket = "terra-state-backend"
-    key = "terraform-play/first-hands-on/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
